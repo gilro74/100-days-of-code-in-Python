e6545ada680f2a761c902b9f3bf32a0df1258341
gilro74 <gilfordmorokolo@gmail.com>
2026-01-01 15:19:34 +0200

    Updated day_02 code with more flexible one

--- Day_02/task02.py
+++ Day_02/task02.py
@@ -1,24 +1,34 @@
 print("Welcome to the tip Calculator!")
 #Note- Keeping same datatype when working with numbers is very important!,like now
 #Working with float only helps in avoiding errors
+
+###############More flexible solution##################
 total_bill = float(input("What is your total bill? $"))
 percentage_tip = float(input("How much tip would you like to give? 10, 12 or 15? "))
+new_total_bill = float(total_bill * (percentage_tip/100) + total_bill)
+number_of_people =float(input("How many people would you like to split the bill? "))
+cost_per_person =new_total_bill / number_of_people
+print(f"Each person should pay ${round(cost_per_person,2)}")
 
-if percentage_tip == 10:
-    new_total_bill = float(total_bill * 1.1) #1.1 means 110%, same with other percentanges
-    number_of_people =float(input("How many people would you like to split the bill? "))
-    cost_per_person =new_total_bill / number_of_people
-    print(round(cost_per_person,2))
+###############first Attempt below############
+
+# if percentage_tip == 10:
+#     new_total_bill = float(total_bill * 1.1) #1.1 means 110%, same with other percentanges
+#     number_of_people =float(input("How many people would you like to split the bill? "))
+#     cost_per_person =new_total_bill / number_of_people
+#     print(round(cost_per_person,2))
     
-elif percentage_tip == 12:
-    new_total_bill = float(total_bill * 1.12)
-    number_of_people =float(input("How many people would you like to split the bill? "))
-    cost_per_person =new_total_bill / number_of_people
-    print(round(cost_per_person,2))
+# elif percentage_tip == 12:
+#     new_total_bill = float(total_bill * 1.12)
+#     number_of_people =float(input("How many people would you like to split the bill? "))
+#     cost_per_person =new_total_bill / number_of_people
+#     print(round(cost_per_person,2))
 
-elif percentage_tip == 15:
-    new_total_bill = float(total_bill * 1.15)
-    number_of_people =float(input("How many people would you like to split the bill? "))
-    cost_per_person =new_total_bill / number_of_people
-    print(round(cost_per_person,2))
-                                                                                                  
+# elif percentage_tip == 15:
+#     new_total_bill = float(total_bill * 1.15)
+#     number_of_people =float(input("How many people would you like to split the bill? "))
+#     cost_per_person =new_total_bill / number_of_people
+#     print(round(cost_per_person,2))
+
+
+                                                                  
